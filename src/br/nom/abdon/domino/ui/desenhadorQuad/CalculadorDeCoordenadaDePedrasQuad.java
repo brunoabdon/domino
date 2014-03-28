5acbd237ad2e3057d41ba495348fcbcea4664614
Bruno Abdon <brunoabdon+github@gmail.com>
2014-03-28 20:43:25 -0300

    falta corrigir muuuuito bug ainda no desenhador.

--- src/br/nom/abdon/domino/ui/desenhadorQuad/CalculadorDeCoordenadaDePedrasQuad.java
+++ src/br/nom/abdon/domino/ui/desenhadorQuad/CalculadorDeCoordenadaDePedrasQuad.java
@@ -44,34 +44,69 @@ public class CalculadorDeCoordenadaDePedrasQuad implements
 		this.y = coordenadaPraDesenhar[1];
 		
 		this.direcao = vaga.getDirecao();
+		if(ehCarroca){
+			
+			switch (this.direcao) {
+			case PRA_DIREITA: this.direcao = Direcao.PRA_BAIXO; break;
+			case PRA_BAIXO: this.direcao = Direcao.PRA_DIREITA; break;
+			case PRA_ESQUERDA: this.direcao = Direcao.PRA_BAIXO; break;
+//			case PRA_pra: this.direcao = Direcao.PRA_BAIXO; break;
+	
+			default:
+				break;
+			}
+		}
 
 	}
 
 	private float[] calculaCoordenadaPraDesenhar(Vaga vaga){
 		
-		//acha a coordenada do pontinho do meio do quadrado
+		//acha a coordenada do pontinho no alto à esquerda do quadrado
 		final float[] coordenadaDoQuadrado = calculaCoordenadaQuadrado(vaga.getLocalizacao());
 		
 		final float xQuadrado = coordenadaDoQuadrado[0];
 		final float yQuadrado = coordenadaDoQuadrado[1];
-		
-		final float[] coordenadaPraDesenhar = coordenadaDoQuadrado;  //hehe. reaproveitando
 
+		float xDesenho = xQuadrado;
+		float yDesenho = yQuadrado; //a principio
+
+		final Direcao direcao = vaga.getDirecao();
+		
+		//se for carroca, desloca um pouquinho pra desenhar. se for normal, desenha ai mesmo
+		
 		if(vaga.ehDeCarroca()){
+			if(direcao == Direcao.PRA_DIREITA){
+				yDesenho = yQuadrado - ladoDeUmQuadrado/2;
+			} else if (direcao == Direcao.PRA_BAIXO){
+				xDesenho = xQuadrado - ladoDeUmQuadrado/2;
+			} else if(direcao == Direcao.PRA_ESQUERDA){
+				xDesenho = xQuadrado + ladoDeUmQuadrado;
+			} else { //Direcao.PRA_CIMA 
+				xDesenho = xQuadrado - ladoDeUmQuadrado/2;
+			}
 			
 		} else {
-			
-		}
-		
-		//acha a coordenada do ponto no alto à esquerda do quadrado
-//		coordenadaPraDesenhar[0] = xQuadrado - ladoDeUmQuadrado/2;
-//		coordenadaPraDesenhar[1] = yQuadrado - ladoDeUmQuadrado/2;
 
+			final boolean andandoNoSentidoDecrescenteDasCoordenadas = 
+					direcao == Direcao.PRA_ESQUERDA || direcao == Direcao.PRA_CIMA;
+
+			if(andandoNoSentidoDecrescenteDasCoordenadas){
+			} else if(direcao == Direcao.PRA_CIMA) {
+				yDesenho = yQuadrado - ladoDeUmQuadrado;
+			} else { //Direcao.PRA_ESQUERDA
+				xDesenho = xQuadrado - ladoDeUmQuadrado;
+			}
+		}
+
+		final float[] coordenadaPraDesenhar = coordenadaDoQuadrado;  //hehe. reaproveitando
+		coordenadaPraDesenhar[0] = xDesenho;
+		coordenadaPraDesenhar[1] = yDesenho;
+		
 		return coordenadaPraDesenhar;
 	}
 
 	private float[] calculaCoordenadaQuadrado(Localizacao localizacao) {
-		return new float[]{ladoDeUmQuadrado/2 + localizacao.getX() * ladoDeUmQuadrado, ladoDeUmQuadrado/2 + localizacao.getY()*ladoDeUmQuadrado};
+		return new float[]{localizacao.getX() * ladoDeUmQuadrado, localizacao.getY()*ladoDeUmQuadrado};
 	}
 
 	@Override
