62261bec2d0d4f4471c3c83db0a4c3365ef8e50a
Bruno Abdon <brunoabdon+github@gmail.com>
2014-04-07 18:28:28 -0300

    denhando uma peca em javafx jah

--- src/br/nom/abdon/domino/ui/fx/DominoApp.java
+++ src/br/nom/abdon/domino/ui/fx/DominoApp.java
@@ -6,17 +6,23 @@
 
 package br.nom.abdon.domino.ui.fx;
 
+import br.nom.abdon.domino.Numero;
 import br.nom.abdon.domino.Pedra;
 import java.util.Collection;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  *
@@ -27,12 +33,26 @@ public class DominoApp extends Application {
     	public static final float LARGURA_DA_MESA = 1260;
 	public static final float ALTURA_DA_MESA = LARGURA_DA_MESA*0.6f;
 
-	public static final float ALTURA_DA_PEDRA = LARGURA_DA_MESA/12;
+	public static final float ALTURA_DA_PEDRA = LARGURA_DA_MESA/8;
 	public static final float LARGURA_DA_PEDRA = ALTURA_DA_PEDRA/2f;
 
 	private static final float afastacaoDaLinha = LARGURA_DA_PEDRA*0.1f;
-	private static final float tamanhoDaLinhaDoMeio = LARGURA_DA_PEDRA - afastacaoDaLinha - afastacaoDaLinha;
-    
+
+      	private static final float raioDosPontinhos = LARGURA_DA_PEDRA/9f;
+	private static final float afastacaoDosPontinhos = LARGURA_DA_PEDRA*0.2f;
+
+	private static final float pontinhosDaEsquerdaX = afastacaoDosPontinhos;
+	private static final float pontinhosDaDireitaX = LARGURA_DA_PEDRA - afastacaoDosPontinhos;
+
+	private static final float pontinhoDoMeioX = (LARGURA_DA_PEDRA/2);
+	private static final float pontinhoDoMeioY = (ALTURA_DA_PEDRA)/4f;
+
+	
+	private static final float pontinhosDaPrimeiraLinhaY = afastacaoDosPontinhos;
+	private static final float pontinhosDaSegundaLinhaY = pontinhoDoMeioY; 
+	private static final float pontinhosDaTerceiraLinhaY =  (ALTURA_DA_PEDRA/2) - afastacaoDosPontinhos;
+
+        
     @Override
     public void start(Stage primaryStage) {
 
@@ -43,7 +63,7 @@ public class DominoApp extends Application {
         
 //        drawShapes(gc);
         
-        Node desenhoPioTerno = criaDesenhoDePedra(Pedra.PIO_TERNO);
+        Node desenhoPioTerno = fazPedra(Pedra.QUINA_SENA);
         desenhoPioTerno.setTranslateX(20);
         desenhoPioTerno.setTranslateY(20);
         
@@ -65,6 +85,10 @@ public class DominoApp extends Application {
         scene.getStylesheets().add(css);
         
         
+        
+        
+        
+        
         primaryStage.show();
 
     }
@@ -81,26 +105,82 @@ public class DominoApp extends Application {
         launch(args);
     }
 
-    private void drawShapes(GraphicsContext gc) {
+    private Node fazPedra(Pedra pedra) {
         
+        Group desenhoPedraEmBranco = fazPedraEmBranco();
+        Group pontinhos = fazPontinhos(pedra);
+
+        Group desenhoPedra = new Group(desenhoPedraEmBranco,pontinhos);
+        desenhoPedra.setId(pedra.name());
+        desenhoPedra.getStyleClass().add("pedra");
+        
+        return desenhoPedra;
         
     }
 
-    private Node criaDesenhoDePedra(Pedra pedra) {
-        
+    private Group fazPedraEmBranco() {
         Rectangle retanguloPedra = new Rectangle(LARGURA_DA_PEDRA, ALTURA_DA_PEDRA);
-        retanguloPedra.getStyleClass().add("pedra");
-        
         Line linhaDoMeio = new Line(0,0,LARGURA_DA_PEDRA-2*afastacaoDaLinha,0);
         linhaDoMeio.translateXProperty().set(afastacaoDaLinha);
         linhaDoMeio.translateYProperty().set(ALTURA_DA_PEDRA/2);
         linhaDoMeio.getStyleClass().add("linhaDePedra");
-        
         Group desenhoPedra = new Group(retanguloPedra,linhaDoMeio);
-        desenhoPedra.setId(pedra.name());
-        
         return desenhoPedra;
+    }
+
+    private Group fazPontinhos(Pedra pedra) {
+        Group pontinhosPrimeiroNumero = fazPontinhos(pedra.getPrimeiroNumero());
+        Group pontinhosSegundoNumero = fazPontinhos(pedra.getSegundoNumero());
+        pontinhosSegundoNumero.setTranslateY(ALTURA_DA_PEDRA/2);
         
+        Group pontinhosDaPEdra = new Group(pontinhosPrimeiroNumero,pontinhosSegundoNumero);
+        return pontinhosDaPEdra;
+        
+    }
+
+    
+    private Group fazPontinhos(final Numero numero) {
+        final Group grupoDePontinhos = new Group();
+        final ObservableList<Node> pontinhos = grupoDePontinhos.getChildren();
+        
+        final int numeroDePontos = numero.getNumeroDePontos();
+        
+        if((numeroDePontos % 2)!= 0){
+            pontinhos.add(fazPontinho(pontinhoDoMeioX, pontinhoDoMeioY));
+        }
+                
+        if(numeroDePontos >= 2){
+            pontinhos.add(fazPontinho(pontinhosDaEsquerdaX, pontinhosDaPrimeiraLinhaY));
+            pontinhos.add(fazPontinho(pontinhosDaDireitaX, pontinhosDaTerceiraLinhaY));
+
+            if(numeroDePontos >= 4){
+                pontinhos.add(fazPontinho(pontinhosDaEsquerdaX, pontinhosDaTerceiraLinhaY));
+                pontinhos.add(fazPontinho(pontinhosDaDireitaX, pontinhosDaPrimeiraLinhaY));
+            
+                if(numeroDePontos == 6){
+                    pontinhos.add(fazPontinho(pontinhosDaEsquerdaX, pontinhosDaSegundaLinhaY));
+                    pontinhos.add(fazPontinho(pontinhosDaDireitaX, pontinhosDaSegundaLinhaY));
+                }
+            
+            }
+        }
+        
+        return grupoDePontinhos;
+        
+    }
+
+    
+    private Shape fazPontinho(float x, float y) {
+        Circle pontinho = fazPontinho();
+        pontinho.setTranslateX(x);
+        pontinho.setTranslateY(y);
+        return pontinho;
+    }
+
+    private Circle fazPontinho() {
+        Circle pontinho = new Circle(raioDosPontinhos);
+        pontinho.getStyleClass().add("pontinho");
+        return pontinho;
     }
     
 }
