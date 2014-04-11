dd424c8437a4bd3b585469b5472286dbc93f74c2
Bruno Abdon <brunoabdon+github@gmail.com>
2014-04-11 19:59:31 -0300

    criando outra hierarquia pra eventlisteners que serao avisados sobre TUDO o que acontece no jogo, e nao apenas aquilo que os jogadores podem saber.
    Loggers e UIs vao preferir seguir esta linha.

---
+++ src/br/nom/abdon/domino/eventos/DominoRootEventListener.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.nom.abdon.domino.eventos;
+
+/**
+ *
+ * @author bruno
+ */
+public interface DominoRootEventListener {
+    
+    /**
+     *
+     * @param eventoSecreto
+     */
+    public void eventoAconteceu(EventoSecreto eventoSecreto);
+    
+    
+}
--- src/br/nom/abdon/domino/motor/util/DominoEventBroadcaster.java
+++ src/br/nom/abdon/domino/motor/util/DominoEventBroadcaster.java
@@ -4,25 +4,43 @@ import java.util.LinkedList;
 import java.util.List;
 
 import br.nom.abdon.domino.eventos.DominoEventListener;
+import br.nom.abdon.domino.eventos.DominoRootEventListener;
 import br.nom.abdon.domino.eventos.EventoDomino;
+import br.nom.abdon.domino.eventos.EventoSecreto;
 
-public class DominoEventBroadcaster implements DominoEventListener {
+public class DominoEventBroadcaster implements DominoEventListener, DominoRootEventListener {
 	
-	private List<DominoEventListener> eventListeners;
+	private final List<DominoEventListener> eventListeners;
+	private final List<DominoRootEventListener> rootEventListeners;
 	
 	public DominoEventBroadcaster() {
-		this.eventListeners = new LinkedList<DominoEventListener>();
+		this.eventListeners = new LinkedList<>();
+                this.rootEventListeners = new LinkedList<>();
 	}
 	
 	public void addEventListneter(DominoEventListener eventListener){
 		this.eventListeners.add(eventListener);
 	}
 	
-	@Override
+	public void addRootEventListneter(DominoRootEventListener rootEventListener){
+		this.rootEventListeners.add(rootEventListener);
+	}
+
+        
+        @Override
 	public void eventoAconteceu(EventoDomino eventoDomino) {
-		for (DominoEventListener eventListener : eventListeners) {
-			eventListener.eventoAconteceu(eventoDomino);
-		}
+            eventListeners.parallelStream().forEach((eventListener) -> {
+                eventListener.eventoAconteceu(eventoDomino);
+            });
+	}
+
+        @Override
+	public void eventoAconteceu(EventoSecreto eventoSecreto) {
+            rootEventListeners.parallelStream().forEach(
+                    (eventListener) -> {
+                        eventListener.eventoAconteceu(eventoSecreto);
+                    }
+            );
 	}
 
 }
