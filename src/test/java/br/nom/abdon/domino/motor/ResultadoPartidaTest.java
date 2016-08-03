87b98b69c1e4523d48aca9c4cd444ed3f2387554
brunoabdon <bru.n.oa.b.don@gmail.com>
2016-08-03 19:17:59 -0300

    adicionando testes

---
+++ src/test/java/br/nom/abdon/domino/motor/ResultadoPartidaTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2016 Bruno Abdon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package br.nom.abdon.domino.motor;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author bamonteiro
+ */
+public class ResultadoPartidaTest {
+    
+    /**
+     * Test of getJogadorRelevante method, of class ResultadoPartida.
+     */
+    @Test
+    public void testEmpate() {
+        assertNotNull(ResultadoPartida.EMPATE);
+        assertNull(ResultadoPartida.EMPATE.getJogadorRelevante());
+    }
+
+    /**
+     * Test of getJogadorRelevante method, of class ResultadoPartida.
+     */
+    @Test
+    public void testGetJogadorRelevante() {
+        System.out.println("getJogadorRelevante");
+        final JogadorWrapper vencedor = UtilsTests.makeJogador();
+        ResultadoPartida rp = new ResultadoPartida(vencedor);
+        assertEquals(rp.getJogadorRelevante(),vencedor);
+
+    }
+    
+}
