aa9c404f91ba9cee6f94415b2461d89eb818f554
Bruno Abdon <brunoabdon+github@gmail.com>
2017-05-29 18:37:17 -0300

    simplificando BASTANTE JogadorQueNaoGostaDeCarroca usando streams

--- src/main/java/com/github/abdonia/domino/exemplos/JogadorQueNaoGostaDeCarroca.java
+++ src/main/java/com/github/abdonia/domino/exemplos/JogadorQueNaoGostaDeCarroca.java
@@ -16,49 +16,76 @@
  */
 package com.github.abdonia.domino.exemplos;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.Comparator;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 import com.github.abdonia.domino.Jogada;
 import com.github.abdonia.domino.Jogador;
 import com.github.abdonia.domino.Lado;
 import com.github.abdonia.domino.Mesa;
-import com.github.abdonia.domino.Numero;
 import com.github.abdonia.domino.Pedra;
 import com.github.abdonia.domino.Vontade;
 
 /**
- * {@link Jogador} que dá prioridade a jogar as carroças. Não tendo carroça, 
- * joga a primeira {@link Pedra} encontrar que caiba na {@link Mesa}.
+ * {@link Jogador} que dá prioridade a jogar as {@link Pedra#isCarroca() 
+ * carroças}. Não tendo carroça, joga a primeira {@link Pedra} encontrar que 
+ * caiba na {@link Mesa}.
  *
  * @author bruno
  *
  */
 public class JogadorQueNaoGostaDeCarroca implements Jogador {
 
-    private Pedra[] carrocas;
-    private Collection<Pedra> naoCarrocas;
-    private int quantasCarrocasEuTenho;
+    private static final BiFunction<Mesa,Pedra,Jogada> JOGADA_MESA = 
+    (m, p) -> 
+        m.taVazia() || p.temNumero(m.getNumeroEsquerda())
+            ? Jogada.jogada(p, Lado.ESQUERDO)
+            : p.temNumero(m.getNumeroDireita())
+                ? Jogada.jogada(p, Lado.DIREITO)
+                :Jogada.TOQUE;
 
-    private Predicate<Pedra> jogavel;
     /**
-     * A  {@link Mesa} em que esse {@link Jogador} 
-     * {@link Jogador#sentaNaMesa(com.github.abdonia.domino.Mesa, int) sentou}
-     * pra jogar.
+     * {@link Comparator} de {@link Pedra} que: (1) diz que qualquer {@link 
+     * Pedra#isCarroca() carroça} é maior que uma não-carroça e (2) carroças 
+     * entre si e não-carroças entre si se comparam pelo {@link 
+     * Pedra#compareTo(java.lang.Enum) comparador natura de pedras}.
      */
-    protected Mesa mesa;
+    private static final Comparator<Pedra> COMP_PREFERE_CARROCA =
+        (p1,p2) -> {
+            final boolean p1EhCarroca = p1.isCarroca();
+            return 
+                p1EhCarroca == p2.isCarroca()
+                    ? p1.compareTo(p2)
+                    : p1EhCarroca ? 1 : -1;
+        };
     
+    /**
+     * {@link Comparator} de {@link Jogada} que diz que: (1) Qualquer jogada é 
+     * maior que {@link Jogada#TOQUE} e (2) Duas jogadas que não são 
+     * <code>TOQUE</code> se diferenciam apenas pelas suas {@link Pedra pedras} 
+     * (ignorando o {@link Lado}) de acordo com o comparador de pedras {@link 
+     * #COMP_PREFERE_CARROCA}.
+     */
+    private static final Comparator<Jogada> COMP_PREFERE_JOGAR_CARROCA =
+        (jog1, jog2) -> 
+            jog1 == jog2
+                ? 0
+                : jog1 == Jogada.TOQUE
+                    ? -1
+                    : jog2 == Jogada.TOQUE
+                        ? 1
+                        : COMP_PREFERE_CARROCA
+                            .compare(jog1.getPedra(), jog2.getPedra())
+    ;
+                    
     /**
      * As {@link Pedra pedras} na mão desse {@link Jogador}.
      */
     protected List<Pedra> mao;
 
-    private boolean perguntouSeEuQueriaJogar;
-
+    private Function<Pedra,Jogada> jogadaNaMesa;
 
     @Override
     public void recebeMao(
@@ -68,32 +95,11 @@ public class JogadorQueNaoGostaDeCarroca implements Jogador {
             final Pedra pedra4,
             final Pedra pedra5,
             final Pedra pedra6) {
-        /* Separando as carroças das nao carroças. 
-           As carroças eu mantenho no array carrocas, cada uma guardada no 
-           indice de seu número (carroça de limpo no carrocas[0], carroça de 
-           terno no carrocas[3], etc...). Onde ficar null, é pq não tenho aquela 
-           carroça.
-           As não-carroças eu passo pra a super, que é um JogadorMamao, e 
-           vai jogar alguma entre elas sempre que eu não tiver carroça pra
-           jogar.
-         */
-        this.naoCarrocas = new ArrayList<>();
-        this.carrocas = new Pedra[7];
-        this.quantasCarrocasEuTenho = 0;
         
-        Arrays.asList(pedra1,pedra2,pedra3,pedra4,pedra5,pedra6).forEach(
-            (pedra) -> {
-                if (pedra.isCarroca()) {
-                    final int indice = 
-                        pedra.getPrimeiroNumero().getNumeroDePontos();
-                    carrocas[indice] = pedra;
-                    quantasCarrocasEuTenho++; //manter esse contador pra ajudar.
-                } else {
-                    naoCarrocas.add(pedra);
-                }
-        });
+        this.mao = 
+            JogadorUtils.fazMao(pedra1, pedra2, pedra3, pedra4, pedra5, pedra6);
     }
-
+    
     /**
      * Joga a maior carroça que puder jogar. Se não tiver carroça que dê pra 
      * jogar, joga como {@link JogadorMamao mamão} memso.
@@ -102,201 +108,43 @@ public class JogadorQueNaoGostaDeCarroca implements Jogador {
      */
     @Override
     public Jogada joga() {
-        Jogada jogada;
-
-        final boolean ehPrimeiraJogadaDaPartida = mesa.taVazia();
-        final boolean ehPrimeiraJogadaDoJogo = 
-                ehPrimeiraJogadaDaPartida && !perguntouSeEuQueriaJogar;
-        final boolean tenhoCarroca = this.quantasCarrocasEuTenho != 0;
-
-        if(ehPrimeiraJogadaDaPartida){
-            if(ehPrimeiraJogadaDoJogo || tenhoCarroca){
-                jogada = fazJogadaDeMaiorCarroca();
-            } else {
-                final Pedra pedra = naoCarrocas.iterator().next();
-                naoCarrocas.remove(pedra);
-                jogada = Jogada.jogada(pedra, Lado.ESQUERDO);
-            }
-        } else if (tenhoCarroca){
-            /* Tenho algumas carroças. Vou ver da pra jogar alguma delas. Vou 
-             dar preferência pra jogar o maior número primeiro. Se não tiver,
-             jogo o menor. Se não tiver, então não tem como jogar carroça. 
-             Jogo como mamão mesmo. */
-            final Lado ladoComMaiorNumero = pegaLadoComMaiorNumero(mesa);
-            jogada = fazJogadaDeAlgumaCarroca(mesa, ladoComMaiorNumero);
-
-            if (jogada == null) {
-                //As carroças que eu tenho não cabem na mesa. Vou jogar como 
-                //mamão mesmo.
-                jogada = fazJogadaDeNaoCarroca();
-            }
-        } else {
-            //nao tenho carrocas....
-            jogada = fazJogadaDeNaoCarroca();
-        }
-        //seja o que Deus quiser.
-        return jogada;
-    }
-
-    private Jogada fazJogadaDeNaoCarroca() {
-        Jogada jogada;
-        //nem tenho carroça mais. simplificando e jogando feito mamão.
-        Pedra pedra = null;
-        Lado lado = null;
-        final Iterator<Pedra> iterator = naoCarrocas.iterator();
-        while(iterator.hasNext()){
-            pedra = iterator.next();
-            if(pedra.temNumero(mesa.getNumeroEsquerda())){
-                lado = Lado.ESQUERDO;
-            } else if (pedra.temNumero(mesa.getNumeroDireita())){
-                lado = Lado.DIREITO;
-            }
-            if(lado != null){
-                iterator.remove();
-                break;
-            }
-        }
-        jogada =
-                lado == null
-                ? Jogada.TOQUE
-                : Jogada.jogada(pedra, lado);
-        return jogada;
-    }
-
-    /**
-     * Diz qual o {@link Lado} da mesa tem o maior {@link Numero}. Se empatar, 
-     * diz qualquer um.
-     * 
-     * @param mesa Uma mesa de uma partida que já começou. A mesa não pode
-     * {@link Mesa#taVazia() estar vazia}.
-     * 
-     * @return O lado da mesa que tem o maior número (ou um lado qualquer se a
-     * mesa estiver fechada.
-     */
-    private Lado pegaLadoComMaiorNumero(final Mesa mesa) {
-        return mesa.getNumeroEsquerda().compareTo(mesa.getNumeroDireita()) > 0 
-            ? Lado.ESQUERDO 
-            : Lado.DIREITO;
-    }
-
-    /**
-     * Retorna uma {@link Jogada} de uma das carroças que tenho, dando 
-     * preferência a um dos {@link Lado}s, caso seja possível jogar uma das 
-     * carroças (retorna <code>null</code> se não rolar).
-     * a ma
-     * @param mesa Uma mesa de uma partida que já começou. A mesa não pode
-     * {@link Mesa#taVazia() estar vazia}.
-     * 
-     * @param ladoPreferencial O lado que deve se dar preferencia pra jogar.
-     * @return Uma jogada de carroça, ou <code>null</code> se não tiver carroça
-     * pra isso.
-     */
-    private Jogada fazJogadaDeAlgumaCarroca(
-            final Mesa mesa, 
-            final Lado ladoPreferencial) {
         
-        Jogada jogada = pegaJogadaDeCarroca(mesa, ladoPreferencial);
-        if (jogada == null) {
-            
-            final Lado ladoDepreciado = 
-                ladoPreferencial == Lado.ESQUERDO 
-                    ? Lado.DIREITO 
-                    : Lado.ESQUERDO;
-            
-            jogada = pegaJogadaDeCarroca(mesa, ladoDepreciado);
-        }
-        return jogada;
-    }
-
-    /**
-     * Faz uma {@link Jogada} inical com a maior carroça que eu tiver.
-     * @return Uma jogada de carroça (sem dizer qual o {@link Lado}, já que é
-     * pra ser jogada inicial).
-     */
-    private Jogada fazJogadaDeMaiorCarroca() {
-        Jogada jogada = null;
-        for (int i = 6; i >= 0; i--) {
-            if (carrocas[i] != null) {
-                jogada = fazJogadaCarroca(i,Lado.ESQUERDO); //qualquer lado
-                break;
-            }
-        }
-        return jogada;
-    }
-
-    /**
-     * Retorna uma {@link Jogada} de uma carroça que eu tenha num dos 
-     * {@link Lado}s da mesa, ou <code>null</code> se eu não tiver a carroça do 
-     * {@link Numero} daquele lado da {@link Mesa}.
-     *
-     * @param mesa A mesa onde se vai jogar. A partida já deve estar rolando e a
-     * mesa não pode {@link Mesa#taVazia() estar vazia}.
-     * 
-     * @param ladoPraJogar O lado da mesa onde é pra jogar.
-     * @return Uma jogada de carroça no lado dado, ou <code>null</code> se não 
-     * tiver a carroça.
-     */
-    private Jogada pegaJogadaDeCarroca(
-            final Mesa mesa, 
-            final Lado ladoPraJogar) {
-
-        final Numero numeroDaCabeca
-            = ladoPraJogar == Lado.ESQUERDO
-                ? mesa.getNumeroEsquerda()
-                : mesa.getNumeroDireita();
-
-        final int indexNoArrayDeCarrocas = numeroDaCabeca.getNumeroDePontos();
-        final Pedra carroca = carrocas[indexNoArrayDeCarrocas];
-
-        return carroca == null
-            ? null
-            : fazJogadaCarroca(indexNoArrayDeCarrocas, ladoPraJogar);
-    }
-
-    /**
-     * Cria uma {@link Jogada} pra a carroça que tá 
-     * no dado indice do array de carroças, em um {@link Lado} específico da 
-     * {@link Mesa}, e tira a carroça do array, pra não usar mais ela. Diminui
-     * também o contador de quantas carroças eu tenho. 
-     * 
-     * @param indexNoArrayDeCarrocas O indice da carroça no array.
-     * @return Uma jogada inical com aquela carroça.
-     */
-    private Jogada fazJogadaCarroca(
-            final int indexNoArrayDeCarrocas, 
-            final Lado ladoPraJogar) {
+        final Jogada jogada =
+            mao
+            .parallelStream()
+            .map(jogadaNaMesa)
+            .max(COMP_PREFERE_JOGAR_CARROCA)
+            .get();
         
-        final Pedra carroca = tiraCarrocaDoArray(indexNoArrayDeCarrocas);
-        return Jogada.jogada(carroca, ladoPraJogar);
-    }
-
-    /**
-     * Tira e retorna a carroça que tá num índice específico do array, 
-     * diminuindo também o contador de quantas carroças eu tenho. 
-     * 
-     * @param indexNoArrayDeCarrocas O indice da carroça no array.
-     * @return A carroça que tava lá.
-     */
-    private Pedra tiraCarrocaDoArray(final int indexNoArrayDeCarrocas) {
-        final Pedra carroca = this.carrocas[indexNoArrayDeCarrocas];
-        this.carrocas[indexNoArrayDeCarrocas] = null;
-        this.quantasCarrocasEuTenho--;
-        return carroca;
+        //mesmo se for TOQUE, nao tem problema. pedra = null;
+        this.mao.remove(jogada.getPedra());
+        
+        return jogada;
+        
     }
 
     @Override
     public void sentaNaMesa(final Mesa mesa, final int cadeiraQueSentou) {
-        this.mesa = mesa;
-        this.perguntouSeEuQueriaJogar = false;
+        this.jogadaNaMesa = p -> JOGADA_MESA.apply(mesa, p);
     }
 
+    /**
+     * Colabora com um possível parceiro da mesma classe: a vonta só empata
+     * caso os dois tenham o memso número de carroças na mão.
+     * @return a vontade de começar a jogar, que vai aumentando de acordo com o
+     * número de carroças na mão.
+     */
     @Override
     public Vontade vontadeDeComecar() {
-        this.perguntouSeEuQueriaJogar = true;
-        return 
-            this.quantasCarrocasEuTenho > 0 
-                ? Vontade.QUERO_MUITO 
-                : Vontade.TANTO_FAZ;
-        
+        final Vontade vontade;
+        switch((int)this.mao.parallelStream().filter(Pedra::isCarroca).count()){
+            case 0:         vontade = Vontade.NAO_QUERO_MESMO;  break;
+            case 1:         vontade = Vontade.NAO_QUERO;        break;
+            case 2:         vontade = Vontade.TANTO_FAZ;        break;
+            case 3:         vontade = Vontade.QUERO;            break;
+            case 4: case 5: case 6: default: //sou o jogador com mais carrocas
+                            vontade = Vontade.QUERO_MUITO;
+        }
+        return vontade;
     }
 }
