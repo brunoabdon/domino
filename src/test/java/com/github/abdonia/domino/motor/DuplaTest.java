12c0733115d354b4e469ac867fac283cbc1cf955
Bruno Abdon <brunoabdon+github@gmail.com>
2017-06-01 18:02:39 -0300

    limpando

--- src/test/java/com/github/abdonia/domino/motor/DuplaTest.java
+++ src/test/java/com/github/abdonia/domino/motor/DuplaTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Bruno Abdon <brunoabdon+github@gmail.com>
+ * Copyright (C) 2016 Bruno Abdon
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,7 +27,7 @@ import com.github.abdonia.domino.Vontade;
 
 /**
  *
- * @author Bruno Abdon <brunoabdon+github@gmail.com>
+ * @author Bruno Abdon
  */
 public class DuplaTest {
     
--- src/test/java/com/github/abdonia/domino/motor/UtilsTests.java
+++ src/test/java/com/github/abdonia/domino/motor/UtilsTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Bruno Abdon <brunoabdon+github@gmail.com>
+ * Copyright (C) 2016 Bruno Abdon
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@ import com.github.abdonia.domino.exemplos.JogadorMamao;
 
 /**
  *
- * @author Bruno Abdon <brunoabdon+github@gmail.com>
+ * @author Bruno Abdon
  */
 public class UtilsTests {
 
