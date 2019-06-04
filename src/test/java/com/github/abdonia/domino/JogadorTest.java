b38e2746b73553bb16c3bcbc0a3d6c45fa12ebc6
Bruno Abdon <brunoabdon+github@gmail.com>
2019-06-04 13:37:30 -0300

    Limpa imports

--- src/test/java/com/github/abdonia/domino/JogadorTest.java
+++ src/test/java/com/github/abdonia/domino/JogadorTest.java
@@ -10,7 +10,6 @@ import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
--- src/test/java/com/github/abdonia/domino/exemplos/JogadorSimplorioTest.java
+++ src/test/java/com/github/abdonia/domino/exemplos/JogadorSimplorioTest.java
@@ -1,10 +1,7 @@
 package com.github.abdonia.domino.exemplos;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 
 import com.github.abdonia.domino.JogadorTest;
 
