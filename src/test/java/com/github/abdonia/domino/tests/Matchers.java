71cc373d980e7ac93be538f8fdd9ce85b78e4bf2
Bruno Abdon <brunoabdon+github@gmail.com>
2019-06-06 14:24:05 -0300

    Cria método pra criar Matcher de pontos de vitoria

--- src/test/java/com/github/abdonia/domino/tests/Matchers.java
+++ src/test/java/com/github/abdonia/domino/tests/Matchers.java
@@ -6,11 +6,15 @@ import static org.hamcrest.Matchers.hasToString;
 
 import org.hamcrest.Matcher;
 
+import com.github.abdonia.domino.Vitoria;
+
 public class Matchers {
 
     public static <X> Matcher<X> hasNonBlankToString(){
         return hasToString(not(blankOrNullString())); 
     }
     
-
+    public static Matcher<Vitoria> vale(final int expectedPontos){
+        return Vale.vale(expectedPontos); 
+    }
 }
