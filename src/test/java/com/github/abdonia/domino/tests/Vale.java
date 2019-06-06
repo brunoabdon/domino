f2c52e38fa6501c196fdb5fd0d201c44f9774a74
Bruno Abdon <brunoabdon+github@gmail.com>
2019-06-06 14:23:48 -0300

    Cria Mathcer de quantos pontos vale uma vitória
    Cria Mathcer pra testar quantos pontos vale uma vitória. Codigo exagerado, feito só pra aprender a escrever um Matcher

---
+++ src/test/java/com/github/abdonia/domino/tests/Vale.java
@@ -0,0 +1,47 @@
+package com.github.abdonia.domino.tests;
+
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+import com.github.abdonia.domino.Vitoria;
+
+public class Vale extends TypeSafeMatcher<Vitoria>{
+
+    private int expectedPontos;
+
+    public static Vale vale(final int expectedPontos) {
+        return new Vale(expectedPontos);
+    }
+    
+    public Vale(final int expectedPontos) {
+        this.expectedPontos = expectedPontos;
+    }
+    
+    @Override
+    protected void describeMismatchSafely(final Vitoria vitoria,
+            final Description mismatchDescription) {
+        
+        mismatchDescription
+            .appendValue(vitoria)
+            .appendText(" vale ")
+            .appendValue(vitoria.getPontos())
+            .appendText(" pontos ");
+        
+    }
+    
+    @Override
+    public void describeTo(final Description description) {
+        
+        description
+            .appendText("uma Vitória valendo ")
+            .appendValue(expectedPontos)
+            .appendText("pontos");
+        
+    }
+
+    @Override
+    protected boolean matchesSafely(final Vitoria vitoria) {
+        return vitoria.getPontos() == expectedPontos;
+    }
+    
+}
